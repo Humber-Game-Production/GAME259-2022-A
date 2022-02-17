b688734bccc143310d43d923b41131ebe7ad2e6c
Trev <daeteri360@gmail.com>
2022-02-17 18:16:56 -0500

    Main Menu & Options Menu added
    Add the main menu widget and the options menu widget. The exit game button is fully functional. The play button opens the current WIP level. The options button takes you to the empty Options menu.
    Also added is a menu player controller that always toggles the mouse cursor, and a main menu game mode, so it does not interfere with the main game mode of the project.

--- GAME259_A_URE/Intermediate/Build/Win64/GAME259_A_UREEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ GAME259_A_URE/Intermediate/Build/Win64/GAME259_A_UREEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30136
-#include "C:/Users/usees/Documents/GitHub/GAME259-2022-A/GAME259_A_URE/Intermediate/Build/Win64/GAME259_A_UREEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30140
+#include "D:/GitHub/GAME259-2022-A/GAME259_A_URE/Intermediate/Build/Win64/GAME259_A_UREEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
