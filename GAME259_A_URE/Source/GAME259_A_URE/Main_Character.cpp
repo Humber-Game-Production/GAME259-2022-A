8f6be066640c8e2ff0eab91bc777709ebc937ad2
Jade Liao <n01446860@humbermail.ca>
2022-03-25 23:49:44 -0400

    Fix CombatStatus Component

--- GAME259_A_URE/Source/GAME259_A_URE/Main_Character.cpp
+++ GAME259_A_URE/Source/GAME259_A_URE/Main_Character.cpp
@@ -58,7 +58,7 @@ AMain_Character::AMain_Character()
 	CurrentHealth = MaxHealth;
 	bReplicates = true;
 	PlayerStatsComp = CreateDefaultSubobject<UPlayerStatsComponent>("PlayerStats");
-
+	CombatStatusComp = CreateDefaultSubobject<UCombatStatusComponent>(TEXT("CombatStatus"));
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 }
