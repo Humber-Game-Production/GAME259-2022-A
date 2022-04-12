dd1fdef619ffc7ade23a6d02e749e0db5de89b6d
Trev <daeteri360@gmail.com>
2022-04-12 13:36:03 -0400

    Merge from Networking

--- GAME259_A_URE/Source/GAME259_A_URE/Private/GrenadeComponent.cpp
+++ GAME259_A_URE/Source/GAME259_A_URE/Private/GrenadeComponent.cpp
@@ -2,16 +2,63 @@
 
 
 #include "GrenadeComponent.h"
+#include "GrenadeActor.h"
+#include "Engine/DataTable.h"
+#include "BallActor.h"
+#include "../Main_Character.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+
 
 UGrenadeComponent::UGrenadeComponent() {
 
 	abilityName = "Grenade";
-	cooldown = 6.0f;
+	cooldown = 1.0f;
 	cd_countdown = 0.0f;
 }
 
-void UGrenadeComponent::TriggerAbilityEffect(){
+bool UGrenadeComponent::TriggerAbilityEffect(){
 
 	UE_LOG(LogTemp, Warning, TEXT("Triggering Ability Effect: %s"), *abilityName.ToString());
-	cd_countdown = cooldown;
+	//SpawnGrenadeActor();
+	AMain_Character* player = (AMain_Character*)GetOwner();
+	if(GetOwner()->HasAuthority())
+	{
+		player->GetCharacterMovement()->AddImpulse(player->GetFollowCamera()->GetComponentRotation().Vector()* 4000.0f, true);
+
+	}
+	return true;
+}
+
+bool UGrenadeComponent::SpawnGrenadeActor()
+{
+	AMain_Character* Player = (AMain_Character*)GetOwner();
+	if (GetOwner()) {
+		UDataTable* datatable = Player->GetBallDataTable();
+		if (datatable) {
+			FBallRow* GrenadeInfo = datatable->FindRow<FBallRow>(FName("Grenade"), TEXT("Grenade"));
+			if (GrenadeInfo) {
+				FActorSpawnParameters ActorSpawnParams;
+				ActorSpawnParams.Owner = GetOwner();
+				ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+				FVector spawnLocation = GetOwner()->GetActorLocation() + FVector(100.0f, 100.0f, 0.0f);
+				FRotator rotation = GetOwner()->GetActorRotation();
+
+				AGrenadeActor* GrenadeActor = GetWorld()->SpawnActor<AGrenadeActor>(
+					AGrenadeActor::StaticClass(),
+					spawnLocation, rotation, ActorSpawnParams);
+
+				if (GrenadeActor) {
+					GrenadeActor->SphereMesh->SetStaticMesh(GrenadeInfo->ballMesh);
+					GrenadeActor->SphereMesh->SetMaterial(0, GrenadeInfo->ballMaterial);
+					GrenadeActor->ApplyImpulse(Player->GetFollowCamera()->GetForwardVector() * 5000.f);
+				}
+			}
+		}
+	}
+
+	
+	
+	return true;
 }
